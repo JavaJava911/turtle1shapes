6434c4be3f97bb1f9c6e3a21dd3afc4455849e9a
Chad Martin <cmartin584@students.mchenry.edu>
2016-09-21 12:56:15 -0500

    Add files via upload

---
+++ turtle2.py
@@ -0,0 +1,15 @@
+import turtle
+Chadly=turtle.Turtle( )
+
+
+Chadly.fd(1000)
+Chadly.rt(90)
+
+Chadly.fd(1000)
+Chadly.rt(90)
+
+Chadly. fd(1000)
+Chadly. rt(90)
+
+Chadly. fd(1000)
+Chadly. rt(90)
---
+++ turtle3.py
@@ -0,0 +1,34 @@
+import turtle
+Chadly=turtle.Turtle( )
+
+def octagon( ):
+            Chadly.fd(50)
+            Chadly.lt(45)
+#sign
+octagon ( )
+
+octagon ( )
+
+octagon ( )
+
+octagon ( )
+
+octagon ( )
+
+octagon ( )
+
+octagon ( )
+
+octagon ( )
+#post
+Chadly.fd(20)
+Chadly.rt(90)
+
+Chadly.fd(200)
+Chadly.lt(90)
+
+Chadly.fd(10)
+Chadly.lt(90)
+Chadly.fd(200)
+
+
